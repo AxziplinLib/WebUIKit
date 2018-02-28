5fa0c09d0c1001bbb665d095144bcfca142db404
devedbox <devedbox@qq.com>
2018-02-28 21:58:28 +0800

    Updated sample codes.

--- WebUI/ViewController.swift
+++ WebUI/ViewController.swift
@@ -7,19 +7,21 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let webVision = WebVision<WKWebView>(configuration: WKWebViewConfiguration())
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
